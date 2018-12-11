d2a26033e5a05e7bfe8b42202db666618f2e2527
trungduc <dangtrungduc17895@gmail.com>
2018-12-11 10:02:30 +0700

    deploy again

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,22 @@ def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
 
-def post_new(request, pk):
+
+def post_new(request):
+	if request.method== 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+			form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form':form})
+
+
+def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method== 'POST':
 		form = PostForm(request.POST, instance=post)
